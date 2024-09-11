f17f31fa17ac69be4d8ecf0e098e60024d551098
Idera Nimah <iderayusuf29@gmail.com>
2024-09-11 13:46:16 +0100

    readme update

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  return (  
+    <>
+    <Header/>
+    </>
+  )
+}
+
+function Header(){
+  return(
+    <>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <ul>
+      <li>Home</li>
+      <li>How it Works</li>
+      <li>Why Flystack</li>
+      <li>Pricing</li>
+      <li>Faq</li>
+    </ul>
+    </>
+  )
+
+}
+
+export default App
+
+     
