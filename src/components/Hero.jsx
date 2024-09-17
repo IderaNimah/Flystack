7c30d138538094aae3fc8108cc2ad07e8263a169
Idera Nimah <iderayusuf29@gmail.com>
2024-09-17 10:47:42 +0100

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,1 +1,13 @@
+function Hero() {
+  return (
+    <div className="Hero">
+      <h1>
+        Deploy SMS Solutions in a <span className="underlined">Jiffy!</span>
+      </h1>
+      <p>Send your automated SMS in an easy, secure and reliable </p>
+      <p> way with our easy-to-use SMS APIs.</p>
+    </div>
+  );
+}
 
+export default Hero;
