447e438f78fe1e462b75d0f1f4bfb0abed3ccac3
Idera Nimah <iderayusuf29@gmail.com>
2024-10-01 12:38:26 +0100

    second slides

--- src/components/mainhow.jsx
+++ src/components/mainhow.jsx
@@ -1,9 +1,28 @@
-function Mainhow(){
-    return(
-        <div className="Mainhow">
-            <h1>Send SMS in 3 Easy Steps</h1>
+function Mainhow() {
+  return (
+    <div className="Mainhow">
+      <div className="img-text">
+        <h1>Send SMS in 3 Easy Steps</h1>
+      </div>
+
+      <div className="img-flex">
+        <div className="img-flex-img">
+          <img src="./src/assets/howitworks.png" alt=".." />
+          <img src="./src/assets/project.png" alt=".." />
+          <img src="./src/assets/status.png" alt=".." />
+          <img src="./src/assets/Heart.png" alt=".." />
+          <img src="./src/assets/Vector.png" alt=".." />
         </div>
-    )
+        <div className="img-flex-text">
+          <h1>Create a Project to get your security credentials</h1>
+          <img src="./src/assets/shapedone.png" alt="" />
+          <img src="./src/assets/shapedtwo.png" alt="" />
+          <img src="./src/assets/shapedthree.png" alt="" />
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Mainhow;
