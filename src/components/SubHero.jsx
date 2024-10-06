c9e3db2b24ec546fb6f23379b130a1e7a7b0b531
Idera Nimah <iderayusuf29@gmail.com>
2024-10-06 12:46:37 +0100

    break and fixed

--- src/components/SubHero.jsx
+++ src/components/SubHero.jsx
@@ -1,12 +1,19 @@
+import Hero from "./Hero";
+
 function SubHero() {
   return (
+    <>
     <div className="SubHero">
       <h1>
         Deploy SMS Solutions in a <span className="underlined">Jiffy!</span>
       </h1>
       <p>Send your automated SMS in an easy, secure and reliable </p>
       <p> way with our easy-to-use SMS APIs.</p>
+
+      <Hero/>
     </div>
+     
+    </>
   );
 }
 
