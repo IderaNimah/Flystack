95127a22c219aadf64c63396bcb1875f16af99cc
Idera Nimah <iderayusuf29@gmail.com>
2024-10-07 15:09:52 +0100

    howitworks

--- src/components/thirdHowItWorks.jsx
+++ src/components/thirdHowItWorks.jsx
@@ -1,19 +1,19 @@
 function ThirdHowItWorks() {
   return (
-    <>
+    <div className="thirdsubMain">
       <div className="thirdsub-text">
         <img src="./assets/Sms.png" alt="..." />
       </div>
 
       <div className="thirdsub-img">
         <h1>Start Sending SMS to your users</h1>
-        <div className="sub-img-flex-img">
+        <div className="subimg-flex-img">
           <img src="./assets/Rectangle 23.png" alt=".." />
           <img src="./assets/Rectangle 24.png" alt=".." />
           <img src="./assets/Rectangle 25.png" alt=".." />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
